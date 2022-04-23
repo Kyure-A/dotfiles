0167ff16541b427baf7d691ada3d459e6def39ec
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-04-23 14:46:35 +0900

    cpp のテンプレートの改善

--- .emacs.d/templates/00:.*.cpp
+++ .emacs.d/templates/00:.*.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ctoi(char) int(char) - 48
-#define REP(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define REP3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
-#define PER(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
-#define PER3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define per(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define per3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
 typedef long long ll;
+typedef long double ld;
 
 int main () {
-
-
-
+$0
 }
