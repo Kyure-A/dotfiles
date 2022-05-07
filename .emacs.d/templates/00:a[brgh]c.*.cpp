0caa6bda2e4e0bc03fdd0c71a78c5b47e1a31de8
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-05-07 20:47:15 +0900

    atcoder 用の C++ テンプレートの変更

---
+++ .emacs.d/templates/00:a[brgh]c.*.cpp
@@ -0,0 +1,38 @@
+/* include */
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <ctime>
+using namespace std;
+/* define */
+#define ctoi(char) int(char) - 48
+#define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
+#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
+#define per(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
+#define per3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
+/* typedef */
+typedef long long ll;
+typedef long double ld;
+/* const */
+const double pi = acos(-1.0);
+
+int main () {
+  $0
+}
