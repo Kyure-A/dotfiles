a2d9e06303f3553f0ee5155111a852e2d338c8ec
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-05-22 13:37:15 +0900

    [Modified] fix wrong command

--- installer.sh
+++ installer.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
 echo -e $'\e[36;5;1mInstall now...\e[m'
+curl -sS https://starship.rs/install.sh | sh
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-curl -sS https://starship.rs/install.sh | zsh
+zplug install
 
 source linkmaker.sh
 echo -e $'\e[36;5;1mdotfiles have just been installed.\e[m'
