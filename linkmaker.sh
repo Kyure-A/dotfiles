ab0ca6b572b6ce752c7d187b2147cf36fb0e910b
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-05-24 01:03:27 +0900

    [Edited] Added proper symbolic links for files in .config/

--- linkmaker.sh
+++ linkmaker.sh
@@ -2,12 +2,18 @@
 
 readonly DOTPATH=~/dotfiles
 
-for f in .??*
+for file in .??*
 do
     [ "$f" = ".git" ] && continue
     [ "$f" = ".gitignore" ] && continue
+    [ "$f" = ".config/" ] && continue
 
-    ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+    ln -snfv "$DOTPATH/$file" "$HOME"/"$file"
 done
 
+for file in .config/*
+do
+    ln -snfv "$DOTPATH/$file" "$HOME"/"$file"
+done
+    
 echo -e $'\e[36;5;1mSymbolic links have just been created.\e[m'
