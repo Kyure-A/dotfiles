493293dca79d22a199e56ce6214c65d6666d3287
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-05-24 19:06:37 +0900

    [Modified] More fashionable link maker prompts

--- linkmaker.sh
+++ linkmaker.sh
@@ -4,16 +4,19 @@ readonly DOTPATH=~/dotfiles
 
 for file in .??*
 do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    [ "$f" = ".config/" ] && continue
+    [ "$file" = ".git" ] && continue
+    [ "$file" = ".gitignore" ] && continue
+    [ "$file" = ".config/" ] && continue
+    [ "$file" = ".config/.config" ] && continue
 
-    ln -snfv "$DOTPATH/$file" "$HOME"/"$file"
+    ln -snf "$DOTPATH/$file" "$HOME"/"$file"
+    echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
 done
 
 for file in .config/*
 do
-    ln -snfv "$DOTPATH/$file" "$HOME"/"$file"
+    ln -snf "$DOTPATH/$file" "$HOME"/"$file"
+    echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
 done
-    
-echo -e $'\e[36;5;1mSymbolic links have just been created.\e[m'
+
+echo -e "\e[32;1mSymbolic Links have just been created!\e[m"
