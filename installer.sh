88bf5113ed8e0e7dfe41db93721fddb7245502f6
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-05-24 19:11:08 +0900

    [Modified] More fashionable installer prompts

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-echo -e $'\e[36;5;1mInstall now...\e[m'
+echo -e "\e[32;1mInstalling..\e[m"
 curl -sS https://starship.rs/install.sh | sh
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 zplug install
 
 source linkmaker.sh
-echo -e $'\e[36;5;1mdotfiles have just been installed.\e[m'
+echo -e "\e[32;1mdotfiles have just been Installed!\e[m"
