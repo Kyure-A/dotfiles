92763e92060c10fbacc71004cf99127a90870a1c
Kyure-A <49436968+Kyure-A@users.noreply.github.com>
2022-05-26 18:27:54 +0900

    [Modified] The process into a nice function.

--- installer.sh
+++ installer.sh
@@ -1,9 +1,20 @@
 #!/bin/zsh
 
 echo -e "\e[32;1mInstalling..\e[m"
-curl -sS https://starship.rs/install.sh | sh
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-zplug install
+
+download()
+{
+    curl -sS https://starship.rs/install.sh | sh
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+}
+
+setup()
+{
+    zplug install
+}
+
+download
+setup
 
 source linkmaker.sh
 echo -e "\e[32;1mdotfiles have just been Installed!\e[m"
--- linkmaker.sh
+++ linkmaker.sh
@@ -2,21 +2,30 @@
 
 readonly DOTPATH=~/dotfiles
 
-for file in .??*
-do
-    [ "$file" = ".git" ] && continue
-    [ "$file" = ".gitignore" ] && continue
-    [ "$file" = ".config/" ] && continue
-    [ "$file" = ".config/.config" ] && continue
+linkmaker()
+{
+    for file in .??*
+    do
+	[ "$file" = ".git" ] && continue
+	[ "$file" = ".gitignore" ] && continue
+	[ "$file" = ".config/" ] && continue
+	[ "$file" = ".config/.config" ] && continue
 
-    ln -snf "$DOTPATH/$file" "$HOME"/"$file"
-    echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
-done
+	ln -snf "$DOTPATH/$file" "$HOME"/"$file"
+	echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
+    done
+}
 
-for file in .config/*
-do
-    ln -snf "$DOTPATH/$file" "$HOME"/"$file"
-    echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
-done
+linkmaker_config()
+{
+    for file in .config/*
+    do
+	ln -snf "$DOTPATH/$file" "$HOME"/"$file"
+	echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
+    done
+}
+
+linkmaker
+linkmaker_config
 
 echo -e "\e[32;1mSymbolic Links have just been created!\e[m"
