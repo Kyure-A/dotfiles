ff425a46beb74a382df79e3530f4b46679167dba
Kyure_A <k@kyre.moe>
2022-07-24 00:34:41 +0900

    [fixed] deleted function (linkmaker_config)

--- linkmaker.zsh
+++ linkmaker.zsh
@@ -4,28 +4,16 @@ readonly DOTPATH=~/dotfiles
 
 linkmaker()
 {
-    for file in .??*
+    for file in .??* # . から始まるファイル/ディレクトリ
     do
 	[ "$file" = ".git" ] && continue
 	[ "$file" = ".gitignore" ] && continue
-	[ "$file" = ".config/" ] && continue
-	[ "$file" = ".config/.config" ] && continue
 
-	ln -snf "$DOTPATH/$file" "$HOME"/"$file"
-	echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
-    done
-}
-
-linkmaker_config()
-{
-    for file in .config/*
-    do
 	ln -snf "$DOTPATH/$file" "$HOME"/"$file"
 	echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
     done
 }
 
 linkmaker
-linkmaker_config
 
 echo -e "\e[32;1mSymbolic Links have just been created!\e[m"
