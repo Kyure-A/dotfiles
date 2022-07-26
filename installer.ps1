581beb7d6f50d0697851001d32c1d3e3078b3fc9
Kyure_A <k@kyre.moe>
2022-07-26 18:29:11 +0900

    [KILLED] powershell [Added] pwsh

---
+++ installer.ps1
@@ -0,0 +1,26 @@
+# Terminal
+winget install gsudo
+winget install Microsoft.WindowsTerminal\
+winget install Microsoft.PowerShell
+Install-Module WslInterop
+Install-Module PSReadLine
+winget install Starship.Starship
+winget install Git.Git
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+# Editors
+winget install XPDCFJDKLZJLP8 # Visual Studio 2022
+winget install Microsoft.VisualStudioCode
+
+#------------------------------------------------------------------------------------------------------------------------------
+
+# Utilitys
+winget install Google.JapaneseIME
+winget install XP89DCGQ3K6VLD #Powertoys
+winget install 9PGCV4V3BK4W #Devtoys
+
+#------------------------------------------------------------------------------------------------------------------------------
+# etc...
+winget install VivaldiTechnologies.Vivaldi
+
