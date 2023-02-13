5474c4b7425a73bee9a97b4819960980d5355f92
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-02-13 23:22:28 +0900

    [Added] next_permutation, [Modified] c++ template

--- .emacs.d/templates/00:.*.cpp
+++ .emacs.d/templates/00:.*.cpp
@@ -1,30 +1,32 @@
-/* include */
-#include<bits/stdc++.h>
+/* ------------------------------ include ------------------------------ */
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+// #include <boost/multiprecision/cpp_int.hpp>
+/* ------------------------------  using  ------------------------------ */
 using namespace std;
-// #include<atcoder/all> using namespace atcoder;
-
-/* Macro */
+// using namespace atcoder;
+// using namespace boost::multiprecision;
+/* ------------------------------  define ------------------------------ */
 #define all(container) (container).begin(), (container).end()
 #define ctoi(char) int(char) - 48
 #define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
 #define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
 #define per(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
 #define per3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
-
-/* Type */
 // #define int long long
-#define cauto const auto&
-typedef long long i64;
-typedef long double ld;
-
-/* Const */
+/* ------------------------------ function ------------------------------ */
+template<typename T> T lcm(T a, T b) {return a / gcd(a, b) * b;}
+/* ------------------------------  const  ------------------------------ */
 const double PI = acos(-1.0);
 const long long  MOD = 1000000007;
 const long long _MOD = 998244353;
-
+/* ------------------------------  code   ------------------------------ */
 
 signed main ()
 {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
   $0
 
   return 0;
