9a45770de962b1d2e65e629b3d1e3c2b15c575f3
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-20 18:39:17 +0900

    [Modified] setting for wsl

--- scripts/installer.zsh
+++ scripts/installer.zsh
@@ -10,10 +10,18 @@ if [ -d "$DOTPATH"] ;then
    
    else
        echo -e "\e[32;1mInstalling...\e[m"
-       source scripts/setup_starship.zsh
-       source scripts/setup_zplug.zsh
-       source scripts/setup_asdf.zsh
-       source scripts/setup_winget.zsh
        source scripts/linkmaker.zsh
+       wait
+       source scripts/setup_starship.zsh
+       wait
+       source scripts/setup_zplug.zsh
+       wait
+       source scripts/setup_asdf.zsh
+       wait
+       if [ -n "$(which wslpath)" ]; then
+	   source scripts/linkmaker_win.zsh
+	   source scripts/setup_winget.zsh
+       fi
+       
        echo -e "\e[32;1mdotfiles have just been Installed!\e[m"
 fi
