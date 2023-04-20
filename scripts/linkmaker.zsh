3fd04394c7f2a1da5c0d08f8be75ed55a4e945ab
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-20 18:32:24 +0900

    [Modified] linkmaker.zsh (.config)

--- scripts/linkmaker.zsh
+++ scripts/linkmaker.zsh
@@ -13,4 +13,12 @@ do
     echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
 done
 
+for file in .config/* # .config 直下のファイル/ディレクトリ
+do
+    [ "$file" = ".config" ] && continue
+
+    ln -snf "$DOTPATH/$file" "$HOME"/"$file"
+    echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
+done
+
 echo -e "\e[32;1mSymbolic Links have just been created!\e[m"
