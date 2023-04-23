a136ac9ff77fcad05133fa1818a7802b7db6e363
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-24 00:41:25 +0900

    [Renamed] scripts/ -> bin/

--- bin/installer.zsh
+++ bin/installer.zsh
@@ -5,22 +5,22 @@ readonly DOTPATH=~/dotfiles
 if [ -d "$DOTPATH"] ;then
    echo -e "\e[32;1mUpdating...\e[m"
    git -C "$DOTPATH" pull
-   source scripts/linkmaker.zsh
+   source bin/linkmaker.zsh
    echo -e "\e[32;1mdotfiles have just been Updated!\e[m"
    
    else
        echo -e "\e[32;1mInstalling...\e[m"
-       source scripts/linkmaker.zsh
+       source bin/linkmaker.zsh
        wait
-       source scripts/setup_starship.zsh
+       source bin/setup_starship.zsh
        wait
-       source scripts/setup_zplug.zsh
+       source bin/setup_zplug.zsh
        wait
-       source scripts/setup_asdf.zsh
+       source bin/setup_asdf.zsh
        wait
        if [ -n "$(which wslpath)" ]; then
-	   source scripts/linkmaker_win.zsh
-	   source scripts/setup_winget.zsh
+	   source bin/linkmaker_win.zsh
+	   source bin/setup_winget.zsh
        fi
        
        echo -e "\e[32;1mdotfiles have just been Installed!\e[m"
