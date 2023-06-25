6bb8d131705cf90f457004c043e3cc316e725abb
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-06-25 18:11:07 +0900

    [Modified] combined setup file

--- bin/setup.zsh
+++ bin/setup.zsh
@@ -1,18 +1,25 @@
 #!/usr/bin/env zsh -xeu
 
-# nodejs (for copilot.el)
+# starship
+if [ -n "$(which starship)"]; then
+    curl -sS https://starship.rs/install.sh | sh
+fi
+
+# zplug
+
+if [ -n "$(which zplug)"]; then
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+    zplug install
+fi
+
+# asdf 
+
+## nodejs (for copilot.el)
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf install nodejs 17.9.1
 asdf local nodejs 17.9.1
 
-# deno (for zeno.zsh)
+## deno (for zeno.zsh)
 asdf plugin add deno
 asdf install deno latest
 asdf local deno latest
-
-# starship
-curl -sS https://starship.rs/install.sh | sh
-
-# zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-zplug install
