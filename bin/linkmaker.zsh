a7710d1d09f9e9a1935ed7478428be3fd3d068d7
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-06-29 15:08:34 +0900

    [Modified] linkmaker

--- bin/linkmaker.zsh
+++ bin/linkmaker.zsh
@@ -4,19 +4,24 @@ DOTPATH=~/dotfiles
 
 for file in .??* # . から始まるファイル/ディレクトリ
 do
+    [ "$file" = ".cargo" ] && continue
+    [ "$file" = ".config" ] && continue
     [ "$file" = ".git" ] && continue
     [ "$file" = ".gitignore" ] && continue
-    [ "$file" = ".config" ] && continue
     [ "$file" = ".tool-versions" ] && continue
-    
+	
     ln -snf "$DOTPATH/$file" "$HOME"/"$file"
     echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
 done
 
 for file in .config/* # .config 直下のファイル/ディレクトリ
 do
-    [ "$file" = ".config" ] && continue
+    ln -snf "$DOTPATH/$file" "$HOME"/"$file"
+    echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
+done
 
+for file in .cargo/* # .cargo 直下のファイル/ディレクトリ
+do
     ln -snf "$DOTPATH/$file" "$HOME"/"$file"
     echo -e " \u2714  \e[32mLinked!\e[m            $DOTPATH/$file -> $HOME/$file"
 done
