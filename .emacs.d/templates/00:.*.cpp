f2fd4f7073d957ca90b79ba69c64ec10dd173c4c
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-07-02 17:07:03 +0900

    [Modified] template for atcoder

--- .emacs.d/templates/00:.*.cpp
+++ .emacs.d/templates/00:.*.cpp
@@ -1,36 +1,22 @@
-/* ------------------------------ include ------------------------------ */
 #include <bits/stdc++.h>
-// #include <atcoder/modint>
-// #include <boost/multiprecision/cpp_int.hpp>
-/* ------------------------------  using  ------------------------------ */
 using namespace std;
+// #include <atcoder/modint>
 // using namespace atcoder;
+// #include <boost/multiprecision/cpp_int.hpp>
 // using namespace boost::multiprecision;
-/* ------------------------------  define ------------------------------ */
 #define all(container) (container).begin(), (container).end()
 #define ctoi(char) int(char) - 48
 #define rep(i,n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define rep3(i,m,n) for (int i = (m); (i) < (int)(n); ++ (i))
-#define per(i,n) for (int i = (int)(n) - 1; (i) >= 0; -- (i))
-#define per3(i,m,n) for (int i = (int)(n) - 1; (i) >= (int)(m); -- (i))
 // #define int long long
-/* ------------------------------ function ------------------------------*/
-template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
-template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
-template<typename T> T lcm(T a, T b) {return a / gcd(a, b) * b;}
-/* ------------------------------  const  ------------------------------ */
 constexpr double PI = acos(-1.0);
-constexpr int dx[4] = {1, 0, -1, 0};
-constexpr int dy[4] = {0, 1, 0, -1};
 constexpr long long  MOD = 1000000007;
 constexpr long long _MOD = 998244353;
-/* ------------------------------   code  ------------------------------ */
 
 signed main ()
 {
   cin.tie(nullptr);
   ios_base::sync_with_stdio(false);
-
+  
   $0
 
   return 0;
