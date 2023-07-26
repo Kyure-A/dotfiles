1b6c476161cdd7149298c808d43049280896e083
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-07-26 21:43:59 +0900

    [Modified] setup script

--- bin/setup.zsh
+++ bin/setup.zsh
@@ -1,27 +1,31 @@
 #!/usr/bin/env zsh -xeu
 
-# starship
+existp() { type "$1" > /dev/null 2>&1; }
 
-if [ ! type "starship" >/dev/null 2>&1 ]; then
-    curl -sS https://starship.rs/install.sh | sh
-fi
+# asdf
+existp "asdf" || git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+existp "asdf" &&
+    {
+	## nodejs
+	asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git;
+	asdf install nodejs latest;
+	
+	## deno
+	asdf plugin add deno
+	asdf install deno latest
+    }
+
+# eask
+existp "eask" || npm install -g @emacs-eask/cli
+
+# rustup
+existp "rustup" || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# starship
+existp "starship" || curl -sS https://starship.rs/install.sh | sh
 
 # zplug
+existp "zplug" || curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-if [ ! type "zplug" >/dev/null 2>&1 ]; then
-    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
-fi
-
-if type "zplug" >/dev/null 2>&1; then
-    zplug install
-fi
-
-# asdf 
-
-## nodejs (for copilot.el)
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install nodejs 17.9.1
-
-## deno (for zeno.zsh)
-asdf plugin add deno
-asdf install deno latest
+existp "zplug" && zplug install
