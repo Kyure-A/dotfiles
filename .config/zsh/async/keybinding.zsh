bf0b8b207639ad90ce1d66afadf6df76ac7906a6
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-09-16 00:14:50 +0900

    feat(zsh): add my plugin (universal-kill-ring)

--- .config/zsh/async/keybinding.zsh
+++ .config/zsh/async/keybinding.zsh
@@ -6,3 +6,5 @@ bindkey '^[[1;5D' backward-word
 bindkey '^[[1;5C' forward-word                     
 bindkey '^H' backward-kill-word # delete previous word with ctrl+backspace
 bindkey '^[[Z' undo # Shift+tab undo last action
+bindkey '^k' copy-line-as-kill
+bindkey '^y' paste-as-yank
