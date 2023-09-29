bc521e59119db9c5470f05471c9fd84959ef9798
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-09-29 23:26:16 +0900

    chore: add next line to script

--- bin/installer.zsh
+++ bin/installer.zsh
@@ -3,23 +3,16 @@
 DOTPATH=~/dotfiles
 
 if [ -d "$DOTPATH" ]; then
-    printf "\033[32;1mUpdating...\033[m"
+    printf "\033[32;1mUpdating...\033[m\n"
     git -C "$DOTPATH" pull
     source bin/linkmaker.zsh
-    if [ type "wslpath" >/dev/null 2>&1 ]; then
-	source bin/winlinkmaker.zsh
-    fi
-    printf "\033[32;1mdotfiles have just been Updated!\033[m"
+    printf "\033[32;1mdotfiles have just been Updated!\033[m\n"
 else
-    printf "\033[32;1mInstalling...\033[m"
+    printf "\033[32;1mInstalling...\033[m\n"
     git clone https://github.com/Kyure-A/dotfiles/ "$DOTPATH"
     source bin/linkmaker.zsh
     wait
     source bin/setup.zsh
-    wait
-    if [ type "wslpath" >/dev/null 2>&1 ]; then
-	source bin/winlinkmaker.zsh
-    fi
-    printf "\033[32;1mdotfiles have just been installed!\033[m"
+    printf "\033[32;1mdotfiles have just been installed!\033[m\n"
     exec $SHELL -l
 fi
