10e65ac87de835854ddf1118762c813c58083f25
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-12-17 04:46:29 +0900

    chore: change zsh history file name

--- .config/zsh/sync/environment.zsh
+++ .config/zsh/sync/environment.zsh
@@ -1,9 +1,9 @@
-HISTFILE=~/.zhistory
-HISTSIZE=100000
-SAVEHIST=100000
+HISTFILE=~/.zsh_history
+HISTSIZE=10000000
+SAVEHIST=10000000
 
-export EDITOR=emacsclient
-export SUDO_EDITOR=emacsclient
+export EDITOR=emacs
+export SUDO_EDITOR=emacs
 export GIT_MERGE_AUTOEDIT=no
 
 WORDCHARS=${WORDCHARS//\/[&.;]} # 単語区切り文字の設定
