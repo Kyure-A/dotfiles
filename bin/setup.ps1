6bf886c18ce948444670469b1500be12e720de2e
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2024-03-11 15:31:17 +0900

    perf: change setting for windows to scoop

--- bin/setup.ps1
+++ bin/setup.ps1
@@ -1,14 +1,32 @@
-winget install gsudo
-winget install Microsoft.WindowsTerminal
-winget install Microsoft.PowerShell
+# install scoop
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+scoop bucket add main
+scoop bucket add extras
+scoop bucket add nonportable
+scoop bucket add ksb https://github.com/Kyure-A/ksb
+
+# install packages
+scoop install main/aria2
+scoop install main/git
+scoop install main/gsudo
+scoop install main/starship
+scoop install main/winfetch
+scoop install extras/archwsl
+scoop install extras/discord
+scoop install extras/powertoys
+scoop install extras/sysinternals
+scoop install extras/vivaldi
+scoop install nonportable/devtoys-np
+scoop install nonportable/google-japanese-input-np
+scoop install ksb/keyhac
+scoop install ksb/nomeiryoui
+scoop install ksb/faithtype-bin
+scoop install ksb/stealth
+
+# execute font setting
+Auto-Patch-Then-Install # faithtype-bin
+nomeiryoui .\setting.ini -set # set noto sans as system font
+
+# install pwsh module
 Install-Module WslInterop
 Install-Module PSReadLine
-winget install Starship.Starship
-winget install Git.Git
-winget install Google.JapaneseIME
-winget install XP89DCGQ3K6VLD #Powertoys
-winget install 9PGCV4V3BK4W #Devtoys
-winget install sysinternals
-winget install nanazip
-winget install rustup
-cargo install eza
